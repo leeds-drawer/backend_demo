c245709d5c97ff1cfbeaea7b826d8dbbfc76a2ce
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-03 01:13:37 +0900

    Create CommentRequestDto.java

---
+++ src/main/java/com/community/community/dto/CommentRequestDto.java
@@ -0,0 +1,11 @@
+// CommentRequestDto.java
+package com.community.community.dto;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommentRequestDto {
+    private String content;
+}
