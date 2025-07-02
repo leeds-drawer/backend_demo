aabddd12bd3239d9cbf8e3ecf526860ce90c2ee0
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-03 01:15:06 +0900

    Update PostRequestDto.java

---
+++ src/main/java/com/community/community/dto/PostRequestDto.java
@@ -0,0 +1,12 @@
+// PostRequestDto.java
+package com.community.community.dto;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostRequestDto {
+    private String title;
+    private String content;
+}
