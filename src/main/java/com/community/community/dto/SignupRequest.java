d99afb664e2c61f215d44e020884a89a0bf6b97e
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-03 01:15:09 +0900

    Update SignupRequest.java

---
+++ src/main/java/com/community/community/dto/SignupRequest.java
@@ -0,0 +1,12 @@
+// SignupRequest.java
+package com.community.community.dto;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SignupRequest {
+    private String username;
+    private String password;
+}
