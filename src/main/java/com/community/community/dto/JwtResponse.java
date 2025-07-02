fb9169f8d03fa5e7d32ff80f1326359707c87858
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-03 01:15:02 +0900

    Update JwtResponse.java

---
+++ src/main/java/com/community/community/dto/JwtResponse.java
@@ -0,0 +1,12 @@
+// JwtResponse.java
+package com.community.community.dto;
+
+import lombok.*;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+    private String username;
+}
