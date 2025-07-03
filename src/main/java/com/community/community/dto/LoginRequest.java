3fb1ebea20dbdacefe561dc0836ce9554d273ed2
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-03 23:59:08 +0900

    Update LoginRequest.java

--- src/main/java/com/community/community/dto/LoginRequest.java
+++ src/main/java/com/community/community/dto/LoginRequest.java
@@ -3,9 +3,7 @@ package com.community.community.dto;
 
 import lombok.*;
 
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class LoginRequest {
     private String username;
     private String password;
