6a71d221139fc17585c6854642b7634ffa48c876
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-03 23:59:00 +0900

    Update CommentResponseDto.java

--- src/main/java/com/community/community/dto/CommentResponseDto.java
+++ src/main/java/com/community/community/dto/CommentResponseDto.java
@@ -1,14 +1,15 @@
 // CommentResponseDto.java
 package com.community.community.dto;
 
+import com.community.community.domain.ParentType;
 import lombok.*;
 
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class CommentResponseDto {
-    private Long id;
-    private String author;
-    private String content;
-    private String createdAt;
+    private Long       id;
+    private String     author;
+    private ParentType parentType;
+    private Long       parentId;
+    private String     content;
+    private String     createdAt;
 }
