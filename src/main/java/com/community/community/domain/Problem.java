3438e701c94b12b83d11ed130c3c0a66ba78d34b
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-04 00:57:48 +0900

    Update Problem.java

--- src/main/java/com/community/community/domain/Problem.java
+++ src/main/java/com/community/community/domain/Problem.java
@@ -6,13 +6,13 @@ import lombok.*;
 
 @Entity @Getter @Setter @NoArgsConstructor
 public class Problem {
-
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /** 백준 문제 번호 (Unique) */
     @Column(unique = true, nullable = false)
-    private Long number;
+    private long number;
 
-    /** 난이도, 제목 등 메타데이터를 가져다 쓸 수도 있음 */
+    public Problem(long number) {           // ← ★추가
+        this.number = number;
+    }
 }
