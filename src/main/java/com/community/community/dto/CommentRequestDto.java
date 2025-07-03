ed889f6cf02779659efe949bde6e7e60a574a9d6
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-03 23:58:58 +0900

    Update CommentRequestDto.java

--- src/main/java/com/community/community/dto/CommentRequestDto.java
+++ src/main/java/com/community/community/dto/CommentRequestDto.java
@@ -1,11 +1,12 @@
 // CommentRequestDto.java
 package com.community.community.dto;
 
+import com.community.community.domain.ParentType;
 import lombok.*;
 
-@Getter @Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
 public class CommentRequestDto {
-    private String content;
+    private ParentType parentType; // SOLUTION / INTERVIEW / QUESTION
+    private Long       parentId;   // 게시물 PK
+    private String     content;
 }
