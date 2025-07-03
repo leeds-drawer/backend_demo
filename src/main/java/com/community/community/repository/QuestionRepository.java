030fe4a9b6c1b9bac35e9a3fa876b9bf3e30ae61
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-04 00:58:12 +0900

    Update QuestionRepository.java

--- src/main/java/com/community/community/repository/QuestionRepository.java
+++ src/main/java/com/community/community/repository/QuestionRepository.java
@@ -1,15 +1,13 @@
 package com.community.community.repository;
 
 import com.community.community.domain.Question;
+import com.community.community.domain.Problem;
+import com.community.community.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Long> {
-
-    /** 문제 번호별 Question 목록 */
-    List<Question> findByProblemNoOrderByCreatedAtDesc(int problemNo);
-
-    /** 문제 번호별 누적 질문 개수 */
-    long countByProblemNo(int problemNo);
+    long countByProblem(Problem problem);
+    List<Question> findByUser(User user);
 }
