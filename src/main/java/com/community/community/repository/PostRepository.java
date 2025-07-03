a989485b27e028bb9009baf025d461a921d4724b
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-03 23:59:31 +0900

    Update PostRepository.java

--- src/main/java/com/community/community/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.community.community.repository;
-import com.community.community.domain.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-    // 필요하다면 커스텀 메서드 추가 가능 (예: findByAuthor…)
-}
