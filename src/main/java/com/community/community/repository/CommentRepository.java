4d07f21d0e4ad3e28a9e56f105286eef69f9a4f3
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-04 00:58:01 +0900

    Update CommentRepository.java

--- src/main/java/com/community/community/repository/CommentRepository.java
+++ src/main/java/com/community/community/repository/CommentRepository.java
@@ -1,7 +1,17 @@
 package com.community.community.repository;
+
 import com.community.community.domain.Comment;
+import com.community.community.domain.ParentType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    // 예: findByPostId 같은 메서드를 정의해도 됨
+
+    /** 특정 게시물(종류+PK) 의 댓글 목록 */
+    List<Comment> findByParentTypeAndParentIdOrderByCreatedAtAsc(
+            ParentType parentType, Long parentId);
+
+    /** 댓글 개수(목록 집계용) */
+    long countByParentTypeAndParentId(ParentType parentType, Long parentId);
 }
