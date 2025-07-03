5d0fe68f9cca78ad628e70728661ebe478408cab
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-03 23:59:44 +0900

    Update UserRepository.java

--- src/main/java/com/community/community/repository/UserRepository.java
+++ src/main/java/com/community/community/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.community.community.repository;
+
 import com.community.community.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // username 으로 회원 조회할 수 있는 메서드 서명
+
     Optional<User> findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
