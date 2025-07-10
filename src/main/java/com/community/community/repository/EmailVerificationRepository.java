92a67c9df23502d7ea53457e5b1935b933136f52
성영준 <69851018+yxun20@users.noreply.github.com>
2025-07-10 10:44:46 +0900

    Create EmailVerificationRepository.java

---
+++ src/main/java/com/community/community/repository/EmailVerificationRepository.java
@@ -0,0 +1,10 @@
+package com.community.community.repository;
+
+import com.community.community.domain.EmailVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmailVerificationRepository extends JpaRepository<EmailVerification, String> {
+    Optional<EmailVerification> findByEmail(String email);
+}
